72fd56fa8a8bf53a05402142b324c11d181c6072
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-11-24 14:22:45 -0300

    Listagem completa

--- src/components/ListTask/index.tsx
+++ src/components/ListTask/index.tsx
@@ -2,15 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './styles.scss'
 import { listarTodos } from 'src/database'
 
-export const ListTask = (props: any) => {
-
-  const [taskList, setTaskList] = useState<any>([])
-
-  useEffect(() => {
-    listarTodos().then((dados) => {
-      setTaskList({ ...dados })
-    })
-  }, [])
+export const ListTask = ({ taskList, atualizarStatusTask }: any) => {
 
   return (
     <div className="listTaskContainer">
@@ -27,7 +19,7 @@ export const ListTask = (props: any) => {
           <tbody>
             {Object.entries(taskList).map((item: any) => (
               <tr key={item[1]._id}>
-                <td><input className="statusCheckbox" type="checkbox" checked={item[1].status} onChange={(event) => { props.atualizarStatusTask(item[1]._id, event.target.checked) }} /></td>
+                <td><input className="statusCheckbox" type="checkbox" checked={item[1].status} onChange={(event) => { atualizarStatusTask(item[1]._id, event.target.checked) }} /></td>
                 <td>{item[1].taskName}</td>
                 <td>{item[1].taskDate}</td>
                 <td>Editar / Apagar</td>
--- src/ui/app/app.tsx
+++ src/ui/app/app.tsx
@@ -4,16 +4,27 @@ import './app.scss'
 import { AddTask } from 'src/components/AddTask'
 import { Title } from 'src/components/Title'
 import { ListTask } from 'src/components/ListTask'
-import { inserir } from 'src/database'
+import { inserir, listarTodos } from 'src/database'
 
 
 export const App: React.FC = () => {
 
   const [taskList, setTaskList] = useState<any>([])
 
+  const listarTasks = () => {
+    listarTodos().then((dados) => {
+      setTaskList({ ...dados })
+    })
+  }
+
+  useEffect(() => {
+    listarTasks()
+  }, [])
+
   const cadastrarTask = (taskDate: Date, taskName: string) => {
     const task = { "status": false, "taskName": taskName, "taskDate": taskDate }
     inserir(task)
+    listarTasks()
   }
 
   const atualizarStatusTask = (index: number, status: boolean) => {
@@ -22,13 +33,14 @@ export const App: React.FC = () => {
     setTaskList(tempTaskList)
   }
 
+
   return (
     <div className="App">
       <Title />
       <AddTask cadastrarTask={cadastrarTask} />
       <hr />
       {/* ScrollView */}
-      <ListTask atualizarStatusTask={atualizarStatusTask} />
+      <ListTask taskList={taskList} atualizarStatusTask={atualizarStatusTask} />
     </div>
   )
 }
